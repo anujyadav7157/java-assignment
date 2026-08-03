50ca9256fc95d60286adfcdebb42b50b63c6aebd
anujyadav7157 <anujyadav0914@gmail.com>
2026-08-03 21:25:20 +0530

    Add files via upload

---
+++ week 1/DigitExtraction.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DigitExtraction {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a three digit number :: ");
+        int number = input.nextInt();
+
+        if (number < 100 || number > 999) {
+            System.out.println("The number is not a three digit number");
+        } else {
+            int hundreds = number / 100;
+            int tens = (number / 10) % 10;
+            int units = number % 10;
+
+            System.out.println("Hundreds : " + hundreds);
+            System.out.println("Tens : " + tens);
+            System.out.println("Units : " + units);
+        }
+
+        input.close();
+    }
+}
